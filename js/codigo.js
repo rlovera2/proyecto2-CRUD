197f2d11b2ec61ef538e2c045585f6459d23ab98
Roberto Lovera <rlovera1cursos@gmail.com>
2023-03-26 06:27:24 -0400

    se programo la funcionalidad de modificar usuario

--- js/codigo.js
+++ js/codigo.js
@@ -4,7 +4,8 @@ const nombre = document.getElementById("nombreInput");
 const apellido = document.getElementById("apellidoInput");
 const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 const cuerpoTabla = document.getElementById("cuerpoTabla");
-let contador = 0;
+let contador = 1;
+
 
 
 /***************** DECLARANDO FUNCIONES *******************/
@@ -12,18 +13,23 @@ let contador = 0;
 const agregarUsuario = () => {
 
     if(document.getElementById("nombreInput").value==""){
-        alert("Por favor ingrese el nombre, gracias.");
-        //document.getElementById("mensaje").style.visibility=visible;
+       // alert("Por favor ingrese el nombre, gracias.");
+        mensaje_nombre.style.display = "inline";
         document.getElementById("nombreInput").focus();
     } 
     
     else if(document.getElementById("apellidoInput").value==""){
-        alert("Por favor ingrese el apellido, gracias.");
+        //alert("Por favor ingrese el apellido, gracias.");
+        mensaje_nombre.style.display = "none";
+        mensaje_apellido.style.display = "inline";
         document.getElementById("apellidoInput").focus();
     } else{
-
+        mensaje_nombre.style.display = "none";
+        mensaje_apellido.style.display = "none";
+        contador = contador + 1;
         const usuario = {
-          id: crypto.randomUUID(),
+          //id: crypto.randomUUID(),
+          id: JSON.stringify(contador),
           nombre: nombre.value,
           apellido: apellido.value,
         };
@@ -33,44 +39,85 @@ const agregarUsuario = () => {
         localStorage.setItem("usuarios", JSON.stringify(usuarios));
         mostrarUsuarios();
         limpiarDatos();
+        
+        alert("Los datos se ingresaron correctamene.");
     }
+  
 };
 
+const editarUsuario = (id,nombre,apellido) => {
+    btnAgregar.style.display = "none";
+    btnActualizar.style.display = "inline";
+    document.getElementById("idInput").value = id;
+    document.getElementById("nombreInput").value = nombre;
+    document.getElementById("apellidoInput").value = apellido;
+    document.getElementById("nombreInput").focus();
+};
+
+
+const actualizarUsuario = () => {
+  const usuario = usuarios.find((usuario) => usuario.id === idInput.value)
+  usuario.nombre = nombre.value;
+  usuario.apellido = apellido.value;
+  localStorage.setItem("usuarios", JSON.stringify(usuarios))
+
+  btnAgregar.style.display = "inline";
+  btnActualizar.style.display = "none";
+
+  idInput.value = "";
+  nombre.value = "";
+  apellido.value = "";
+
+  mostrarUsuarios();
+}
+
+const eliminarUsuario = (id) =>{
+  const usuario = usuarios.find((usuario) => usuario.id === id)
+  const index = usuarios.indexOf(usuario)
+  usuarios.splice(index, 1)
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarUsuarios();
+};
 
 const limpiarDatos = () => {
     document.getElementById("nombreInput").value = "";
     document.getElementById("apellidoInput").value = "";
     document.getElementById("nombreInput").focus();
+    x = 0;
 };
 
 
 const mostrarUsuarios = () => {
-    cuerpoTabla.innerHTML = "";
-    usuarios.forEach((usuario) => {
-      cuerpoTabla.innerHTML += `<tr>
-          <th scope="row">${usuario.id}</th>
-          <td>${usuario.nombre}</td>
-          <td>${usuario.apellido}</td>
-          <td>
-          <button
-            type="button"
-            class="btn btn-danger"
-            onclick="actualizarUsario('${usuario.id}')"
+  cuerpoTabla.innerHTML = "";
+  usuarios.forEach((usuario) => {
+    cuerpoTabla.innerHTML += `<tr>
+        <th scope="row" class="registros">
+        <a href="#" 
+        onclick="editarUsuario('${usuario.id}','${usuario.nombre.toUpperCase()}','${usuario.apellido.toUpperCase()}')"
           >
-            Actualizar
-          </button>
-          <button
-            type="button"
-            class="btn btn-danger"
-            onclick="eliminarUsario('${usuario.id}')"
-          >
-            Eliminar
-          </button><td>
-      </tr>`;
-    });
-  };
-  
+        ${usuario.id}</a></th>
 
-const eliminarUsuario = (id) =>{
-  localStorage.removeItem(id);
-}
+        <td class="registros">${usuario.nombre.toUpperCase()}</td>
+        <td class="registros">${usuario.apellido.toUpperCase()}</td>
+        <td class="registros">
+        <button
+          type="button"
+          class="botones"
+          onclick="editarUsuario('${usuario.id}','${usuario.nombre.toUpperCase()}','${usuario.apellido.toUpperCase()}')"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          class="botones"
+          onclick="eliminarUsuario('${usuario.id}')"
+        >
+          Eliminar
+        </button></td>
+    </tr>`;
+  });
+};
+
+
+
+window.addEventListener("load", mostrarUsuarios);
